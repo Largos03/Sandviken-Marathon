80f7e77c3fd5f174d8a1ea104ef7793d80f78bd5
Largos03 <127496162+Largos03@users.noreply.github.com>
2025-04-01 09:29:13 +0200

    First
    First push

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import "./style.css";
+  </script>
+  
+  <header>
+	<nav>
+	  <div class="logo">
+		<a href="/">
+		  <img src="/Logo.webp" alt="Sandviken Marathon Logo" />
+		</a>
+	  </div>
+	  <ul class="menu">
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/course">Course</a></li>
+		<li><a href="/register">Register</a></li>
+		<li><a href="/results">Results</a></li>
+		<li><a href="/contact">Contact</a></li>
+	  </ul>
+	</nav>
+  </header>
+  
+  <main>
+	<slot />
+  </main>
+  
+  <footer>
+	<p>&copy; 2025 Stockholm Marathon. All rights reserved.</p>
+  </footer>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    // Add JavaScript here if needed
+  </script>
+  
+  <section class="hero"></section>
+  
+  <div class="hero-content" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+    <h1>SANDVIKEN MARATHON</h1>
+    <p style="font-size: 20px">Mar 01, 2025</p>
+  </div>
+
+<div class="container" >
+
+  <section class="event-details">
+    <h2>About the Race</h2>
+    <p>
+      The adidas Sandviken Marathon takes you through the streets of a beautiful European capital built on 14 islands. The course offers spectacular views of a unique city with varied scenery, including nature and an impeccably preserved medieval city centre.
+    </p>
+    <a href="/about" class="read-more">Learn More</a>
+  </section>
+  
+  <section class="course-details">
+    <h2>The Course</h2>
+    <img src="/sandviken map.jpg" alt="Map of Sandviken" style="display: block; height: 300px; width: 500px; align-self: center; margin: 0 auto;" />
+    <p>
+      Experience a scenic route that showcases the beauty of Sandviken, passing iconic landmarks and picturesque landscapes.
+    </p>
+    <a href="/course" class="read-more">Explore the Course</a>
+  </section>
+  
+  <section class="results">
+    <h2>Results</h2>
+    <p>
+      Check out the results from previous marathons and see how participants performed over the years.
+    </p>
+    <a href="/results" class="read-more">View Results</a>
+  </section>
+
+</div>
